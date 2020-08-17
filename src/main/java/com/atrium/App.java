f4cf508268f25176e6f4dc15bef3ba27dc8c9434
Ash <hrksingh17@gmail.com>
2020-08-17 22:26:47 +0530

    CRUD ops

--- src/main/java/com/atrium/App.java
+++ src/main/java/com/atrium/App.java
@@ -1,7 +1,10 @@
 package com.atrium;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.support.DaoSupport;
 
 import com.atrium.dao.StudentDao;
 import com.atrium.entities.Student;
@@ -15,9 +18,25 @@ public class App
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/atrium/config.xml");
         StudentDao studentdao = context.getBean("studentDao",StudentDao.class );
-        Student s = new Student(1,"Ash","Delhi");
-        Integer r =studentdao.insert(s);
-        System.out.println(r);
-
+        
+       System.out.println("-----------------------------------------------------------read---------------------------------------------------------------------------------------");
+        //read
+        Student student = studentdao.getStudent(2);
+        System.out.println(student);
+        
+        System.out.println("-----------------------------------------------------------read all------------------------------------------------------------------------------------");
+        //readAll
+        List<Student> allStudents = studentdao.getAllStudents();
+        System.out.println(allStudents);
+        
+        System.out.println("-----------------------------------------------------------update------------------------------------------------------------------------------------");
+        Student student2 = studentdao.getStudent(3);
+        student2.setStudentId(3);
+        student2.setStudentName("Ram");
+        student2.setStudentCity("Alaska");
+        studentdao.updateStudent(student2);
+        
+        System.out.println("-----------------------------------------------------------delete------------------------------------------------------------------------------------");
+        System.out.println(studentdao.deleteStudent(3));
     }
 }
--- src/main/java/com/atrium/dao/StudentDao.java
+++ src/main/java/com/atrium/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.atrium.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -17,11 +19,41 @@ public class StudentDao {
 	@Setter
 	private HibernateTemplate hibernateTemplate;
 
-	//save Student
+	// save Student :- create
 	@Transactional
 	public int insert(Student student) {
 		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
+
+	}
+
+	// read:- getting single entry
+	public Student getStudent(int studentId) {
+		return this.hibernateTemplate.get(Student.class, studentId);
+	}
+
+	// get all entries
+	public List<Student> getAllStudents() {
+		return this.hibernateTemplate.loadAll(Student.class);
+
+	}
+
+	// update entry
+	@Transactional
+	public void updateStudent(Student student) {
+		this.hibernateTemplate.update(student);
+
+	}
+
+	// delete the entry
+	@Transactional
+	public boolean deleteStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		if (student != null) {
+			this.hibernateTemplate.delete(student);
+			return true;
+		}
+		return false;
 
 	}
 
